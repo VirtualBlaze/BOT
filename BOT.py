fdddb68954279bde9637a028a199393770f39831
VirtualBlaze <33165250+VirtualBlaze@users.noreply.github.com>
2017-10-27 23:09:06 +0200

    bot.py

---
+++ BOT.py
@@ -0,0 +1,10 @@
+###############################################
+#
+#	Nombre: Mari Eli
+#	Función: idk
+#	Autor: Alesito y Adansito
+#	Fecha de inicio: 27/10/2017
+#	Versión: 0.1
+#	Notas:
+#
+##############################################
